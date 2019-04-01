9be80be3df3c8d2ed1872c360a0df1cd626d0ed2
DESKTOP-SIINF0D\takay <hideto-t-17e@stu.kbc.ac.jp>
2019-04-01 11:56:36 +0900

    Revert "Revert "上書き""
    This reverts commit 5aefb590cb170504f5c45a7af270fe0b74ed6524.

---
+++ Assets/hideto/GetApple_11_TakayasuHideto/Assets/TimeSC.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class TimeSC : MonoBehaviour {
+
+    private float time = 60;
+
+	// Use this for initialization
+	void Start () {
+        // 初期値の60秒
+        GetComponent<Text>().text = ((int)time).ToString();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // 一秒ずつ減らす
+        time -= Time.deltaTime;
+        // マイナス無し
+        if (time < 0) time = 0;
+        GetComponent<Text>().text = ((int)time).ToString();
+
+        // 時間が0になったらゲームオーバー
+        if (time == 0)
+        {
+            SceneManager.LoadScene("GameOver");
+        }
+    }
+}
