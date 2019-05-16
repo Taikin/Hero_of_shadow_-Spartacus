8592434d0154fa5eab0f3363041946a973f6f8fe
MYCOMPUTER\山城主水 <mondo-y-17e@stu.kbc.ac.jp>
2019-05-16 11:45:32 +0900

    <追加>クリア時の際、仲間が敵を追いかける処理

---
+++ Assets/masato/Script/FriendController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FriendController : MonoBehaviour
+{
+    private EscapeEnemyController escapeController;
+    private Animator animator;
+    private GameObject escapeEnemy;
+
+    public GameObject _EscapeEnemy { set { escapeEnemy = value; } }
+
+	void Start ()
+    {
+        escapeController =escapeEnemy.GetComponent<EscapeEnemyController>();
+        animator = GetComponent<Animator>();
+	}
+	
+	void Update ()
+    {
+        if (escapeController._STATE == EscapeEnemyController.STATE._ESCAPE)
+        {
+            Debug.Log("ESCAPE");
+            animator.SetBool("Chase", true);
+            transform.position += transform.forward * Time.deltaTime;
+        }
+    }
+}
