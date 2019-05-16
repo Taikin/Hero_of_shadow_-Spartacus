dbe6063d58f43d351dbe40a8767cfd01a28a177c
DESKTOP-SIINF0D\takay <hideto-t-17e@stu.kbc.ac.jp>
2019-05-16 15:11:23 +0900

    [更新]受け取り

---
+++ Assets/Main/Main_SC/FriendController_Main.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FriendController_Main : MonoBehaviour {
+    private EscapeEnemyController_Main escapeController;
+    private Animator animator;
+    private GameObject escapeEnemy;
+    private float timer;
+
+    public GameObject _EscapeEnemy { set { escapeEnemy = value; } }
+
+    void Start()
+    {
+        escapeController = escapeEnemy.GetComponent<EscapeEnemyController_Main>();
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (escapeController._STATE == EscapeEnemyController_Main.STATE._ESCAPE)
+        {
+            timer += Time.deltaTime;
+            if (timer > 0.2f)
+            {
+                Debug.Log("ESCAPE");
+                animator.SetBool("Chase", true);
+                transform.position += transform.forward * Time.deltaTime;
+            }
+        }
+    }
+}
