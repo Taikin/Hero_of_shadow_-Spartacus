9b50ca0ead467db5ceacbdf8b5d2b4f269788f96
DESKTOP-6MO8KG5\owner-taiki <taiki-h-17e@stu.kbc.ac.jp>
2019-05-17 10:56:56 +0900

    影のやられアニメーションと実体の連動

---
+++ Assets/taiki/Script/deletesub.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class deletesub : MonoBehaviour {
+    private float spaDeleteflg;
+    public float spaDeleteFlg { set { spaDeleteflg = value; } }
+    Animator animator;
+	// Use this for initialization
+	void Start () {
+        animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (spaDeleteflg != 0)
+        {
+            animator.SetBool("deletesub", true);
+        }
+	}
+}
